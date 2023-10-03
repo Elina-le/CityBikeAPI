58a3049aa9743074c4e7a5accd4f33870adbad44
Elina Lepistö <elina.lepisto@student.careeria.fi>
2023-10-03 09:42:23 +0300

    Add controller and Get method for Journeys
    -Hard-coded limit: 100

---
+++ CityBikeAPI/Controllers/JourneysController.cs
@@ -0,0 +1,36 @@
+﻿using CityBikeAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityBikeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JourneysController : ControllerBase
+    {
+        private readonly CityBikeDbContext _db;
+
+        public JourneysController(CityBikeDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public ActionResult GetAllJourneys()
+        {
+            try
+            {
+                List<Journey> journeys = _db.Journeys.Take(100).ToList();
+                return Ok(journeys);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Something went wrong: " + e.Message);
+            }
+        }
+
+
+
+    }
+}
