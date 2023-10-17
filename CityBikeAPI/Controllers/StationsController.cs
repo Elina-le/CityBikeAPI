17e68416bb8ca35cae0b120195fcf4ba8f8b994b
Elina Lepistö <elina.lepisto@student.careeria.fi>
2023-10-17 15:31:01 +0300

    Add calculations to the GetStationCalculations endpoint
    -Average distance of a journey starting from the station
    -Average distance of a journey ending at the station
    -The top 5 most popular return stations for journeys starting from the station
    -The 5 most popular departure stations for journeys ending at the station

--- CityBikeAPI/Controllers/StationsController.cs
+++ CityBikeAPI/Controllers/StationsController.cs
@@ -57,13 +57,61 @@ namespace CityBikeAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult GetStationCalculations(int id)
         {
+            // Average distance of a journey starting from the station
+            var departureAverageDistance = _db.Journeys
+                .Where(j => j.DepartureStationId == id)
+                .Average(j => (double?)j.CoveredDistanceM);
+
+            decimal departureAverageDistanceInKilometers = (decimal)(departureAverageDistance.GetValueOrDefault() / 1000.0);
+            decimal roundedAverageDistanceInKilometers_departure = Math.Round(departureAverageDistanceInKilometers, 2);
+            
+            // Average distance of a journey ending at the station
+            var returnAverageDistance = _db.Journeys
+               .Where(j => j.ReturnStationId == id)
+               .Average(j => (double?)j.CoveredDistanceM);
+
+            decimal returnAverageDistanceInKilometers = (decimal)(returnAverageDistance.GetValueOrDefault() / 1000.0);
+            decimal roundedAverageDistanceInKilometers_return = Math.Round(returnAverageDistanceInKilometers, 2);
+
+            // The top 5 most popular return stations for journeys starting from the station.
+            var top5ReturnStations = _db.Journeys
+                    .Where(j => j.DepartureStationId == id)
+                    .GroupBy(j => j.ReturnStationId)
+                    .OrderByDescending(group => group.Count())
+                    .Take(5)
+                    .Select(group => new
+                    {
+                        returnStationId = group.Key,
+                        returnStationName = group.First().ReturnStationName,
+                        numberOfReturns = group.Count()
+                    })
+                    .ToList();
+
+            //The 5 most popular departure stations for journeys ending at the station.
+            var top5DepartureStations = _db.Journeys
+                    .Where(j => j.ReturnStationId == id)
+                    .GroupBy(j => j.DepartureStationId)
+                    .OrderByDescending(group => group.Count())
+                    .Take(5)
+                    .Select(group => new
+                    {
+                        departureStationId = group.Key,
+                        departureStationName = group.First().DepartureStationName,
+                        numberOfDepartures = group.Count()
+                    })
+                    .ToList();
+
             try
             {
                 var stationCalculations = new
                 {
                     Id = id,
                     Departures = _db.Journeys.Count(j => j.DepartureStationId == id),
-                    Returns = _db.Journeys.Count(j => j.ReturnStationId == id)
+                    Returns = _db.Journeys.Count(j => j.ReturnStationId == id),
+                    AverageDistanceStartingFromStation = roundedAverageDistanceInKilometers_departure,
+                    AverageDistanceEndingAtStation = roundedAverageDistanceInKilometers_return,
+                    Top5ReturnStations = top5ReturnStations,
+                    Top5DepartureStations = top5DepartureStations,
                 };
 
                 if (stationCalculations != null)
