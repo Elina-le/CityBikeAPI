b0f4dce9f1b528bfbd0d70117b02f1f9dadb0941
Elina Lepistö <elina.lepisto@student.careeria.fi>
2023-11-06 13:26:13 +0200

    Add Get method to retrieve paginated journeys
    -Calculate total count of journeys in database
    -Calculate total number of pages required to display data based on the pageSize
    -Validate incoming parameters to ensure they are valid for pagination
    -Retrieve a specific page of data from the database
    -Skip method skips records based on the page and pageSize, and Take retrieves the number of records specified by pageSize
    -Set response headrer to provide information about the pagination
    -Comment out the original GetAll journeys method

--- CityBikeAPI/Controllers/JourneysController.cs
+++ CityBikeAPI/Controllers/JourneysController.cs
@@ -1,6 +1,7 @@
 ﻿using CityBikeAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CityBikeAPI.Controllers
 {
@@ -16,20 +17,53 @@ namespace CityBikeAPI.Controllers
         }
 
 
-        [HttpGet]
-        public ActionResult GetAllJourneys()
-        {
-            try
-            {
-                List<Journey> journeys = _db.Journeys.Take(100).ToList();
-                return Ok(journeys);
-            }
-            catch (Exception e)
-            {
-                return BadRequest("Something went wrong: " + e.Message);
-            }
-        }
+        //[HttpGet]
+        //public ActionResult GetAllJourneys()
+        //{
+        //    try
+        //    {
+        //        List<Journey> journeys = _db.Journeys.Take(100).ToList();
+        //        return Ok(journeys);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return BadRequest("Something went wrong: " + e.Message);
+        //    }
+        //}
 
+
+        [HttpGet]
+        public IActionResult GetPaginatedJourneys(int page = 0, int pageSize = 100)
+        {
+            var totalRecords = _db.Journeys.Count();
+            var totalPages = (int)Math.Ceiling((double)totalRecords / pageSize);
+
+            //Request validation:
+            if (page < 0)
+            {
+                return BadRequest("Invalid page number. Page number must be greater than or equal to 1.");
+            }
+
+            if (page > totalPages)
+            {
+                return BadRequest($"Requested page {page} exceeds the total number of pages ({totalPages}).");
+            }
+
+            if (pageSize <= 0)
+            {
+                return BadRequest("Invalid page size. Page size must be greater than 0.");
+            }
+
+            var query = _db.Journeys.Skip((page) * pageSize).Take(pageSize);
+            var data = query.ToList();
+
+            //Return information about the pagination in the response headers:
+            Response.Headers.Add("X-Total-Count", totalRecords.ToString());
+            Response.Headers.Add("X-Total-Pages", totalPages.ToString());
+
+            return Ok(data);
+
+        }
 
 
     }
