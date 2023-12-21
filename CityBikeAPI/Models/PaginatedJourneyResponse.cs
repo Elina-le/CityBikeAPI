c86fa541abeed85d235b170aad87aa18d31f892b
Elina Lepistö <elina.lepisto@student.careeria.fi>
2023-12-21 09:08:51 +0200

    Delete unnecessary class
    -This was an alternative way to send the number of pages, but I used response headers instead

--- CityBikeAPI/Models/PaginatedJourneyResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CityBikeAPI.Models
-{
-    public class PaginatedJourneyResponse<T>
-    {
-        public int TotalRecords { get; set; }
-        public int TotalPages { get; set; }
-        public IEnumerable<T> ?Data { get; set; }
-    }
-}
